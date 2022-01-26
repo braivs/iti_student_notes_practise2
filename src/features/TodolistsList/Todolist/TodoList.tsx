98f7727c5a2a02a2f9e8901cb82fbe023e26eb6d
Briws <brightwiths@gmail.com>
2022-01-26 09:25:14 +0300

    Connecting styles

--- src/features/TodolistsList/Todolist/TodoList.tsx
+++ src/features/TodolistsList/Todolist/TodoList.tsx
@@ -1,36 +1,23 @@
 import React from 'react';
+//import from regular style
+import './Todolist.css'
+//import from encapsulated style
+import styles from './Todolist.module.css'
 
 export function Todolist(props: PropsType) {
     // drawing props
     return(
         <div>
-            <div>{props.title1}</div>
-            <div>{props.title2}</div>
-            <div>{props.title3}</div>
-            <div>{props.title4}</div>
+            {/*applying from regular style*/}
+            <div className={'MyStyle'}>{props.title}</div>
+            {/*applying from encapsulated style*/}
+            <div className={styles.MyStyle}>{props.title}</div>
+            {/*applying TWO style throw template strings (Ё button) */}
+            <div className={`${styles.MyStyle1} ${styles.MyStyle2}`}>{props.title}</div>
         </div>
     )
 }
-
-//using destructuring                         ...and other props
-export function TodolistDest({title1, title2, ...props}: PropsType) {
-    // drawing props
-    return(
-        <div>
-            <div>{title1}</div> {/*respectively directly without props*/}
-            <div>{title2}</div> {/*respectively directly without props*/}
-            <div>{props.title3}</div> {/*and here ... other props*/}
-            <div>{props.title4}</div> {/*and here ... other props*/}
-        </div>
-    )
-}
-
-const props = {id: 10, age: 20}
-const {id: a, age: b} = props // give the name to variable-> will be a,b
 
 type PropsType = {
-    title1: string
-    title2: string
-    title3: string
-    title4: string
+    title: string
 }
