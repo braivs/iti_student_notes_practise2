fef68b10bdde3b6787aad1bffc1772ad2a3ea822
Briws <brightwiths@gmail.com>
2022-01-26 09:12:26 +0300

    Destructuring of props

--- src/features/TodolistsList/Todolist/TodoList.tsx
+++ src/features/TodolistsList/Todolist/TodoList.tsx
@@ -1,15 +1,36 @@
-//"Classic typing" - as need to do
-export function TodolistClassicTipisation(props: PropsType) {}
-//"fast typing" - no NEED to so this :) but need to understand
-export function TodolistFastTipisation(props: { title: string, tasks: Array<TaskType>}) {}
+import React from 'react';
 
-type PropsType = {
-    title: string
-    tasks: Array<TaskType>
+export function Todolist(props: PropsType) {
+    // drawing props
+    return(
+        <div>
+            <div>{props.title1}</div>
+            <div>{props.title2}</div>
+            <div>{props.title3}</div>
+            <div>{props.title4}</div>
+        </div>
+    )
 }
 
-export type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
+//using destructuring                         ...and other props
+export function TodolistDest({title1, title2, ...props}: PropsType) {
+    // drawing props
+    return(
+        <div>
+            <div>{title1}</div> {/*respectively directly without props*/}
+            <div>{title2}</div> {/*respectively directly without props*/}
+            <div>{props.title3}</div> {/*and here ... other props*/}
+            <div>{props.title4}</div> {/*and here ... other props*/}
+        </div>
+    )
+}
+
+const props = {id: 10, age: 20}
+const {id: a, age: b} = props // give the name to variable-> will be a,b
+
+type PropsType = {
+    title1: string
+    title2: string
+    title3: string
+    title4: string
 }
